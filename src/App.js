7c03a76fdb08ad44e836bab515c4ae573f355b05
anishazahan <anishazahan13@gmail.com>
2022-07-07 18:21:56 +0600

    environment setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import logo1 from '../src/img/retina-logo3.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2 className='text-3xl text-primary'>Hellow</h2>
+     <div class="">
+      <img src={logo1} alt="" />
+     </div>
     </div>
   );
 }
