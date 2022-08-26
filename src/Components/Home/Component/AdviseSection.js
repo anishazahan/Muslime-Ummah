376d3d7787d343d6993d6ce7545c31bffa2089fb
anishazahan <anishazahan13@gmail.com>
2022-08-26 20:18:13 +0600

    advise sections content design

--- src/Components/Home/Component/AdviseSection.js
+++ src/Components/Home/Component/AdviseSection.js
@@ -7,28 +7,59 @@ const AdviseSection = () => {
   return (
     <>
 
-        <div className="max-width mt-10">
+        <div className="max-width my-20">
             <div className=" flex flex-col lg:flex-row px-16">
-                <div className="left flex flex-col w-full">
+                <div className="left flex flex-col w-full space-y-8">
 
                     <div className="quote flex">
                         <FaQuoteLeft className='text-primary font-bold text-5xl mr-3'></FaQuoteLeft>
                         <h1 className="text-3xl font-semibold">Take Good Advice From People Who Pronounce It Though He Does Not Practice</h1>
                     </div>
-                    <h1 className="quote italic">
+                    <h1 className="quote italic leading-8">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod diti tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimsa veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat. 
                         </h1>
-                        <div className="">
-                            <div className="flex">
-                                <FaMosque></FaMosque>
-                                <p>Join The Islamic Community</p>
+
+                        <div className="flex flex-col md:flex-row">
+                        <div className=" mr-8 space-y-2">
+                            <div className="flex items-baseline">
+                                <FaMosque className='text-primary font-bold mr-2'></FaMosque>
+                                <p className=''>Join The Islamic Community</p>
+                            </div>
+                            <div className="flex items-baseline">
+                                <FaMosque className='text-primary font-bold mr-2'></FaMosque>
+                                <p className=''>Join The Islamic Community</p>
+                            </div>
+                            <div className="flex items-baseline">
+                                <FaMosque className='text-primary font-bold mr-2'></FaMosque>
+                                <p className=''>Join The Islamic Community</p>
                             </div>
                         </div>
+                        <div className="space-y-2">
+                            <div className="flex items-baseline">
+                                <FaMosque className='text-primary font-bold mr-2'></FaMosque>
+                                <p className=''>Join The Islamic Community</p>
+                            </div>
+                            <div className="flex items-baseline">
+                                <FaMosque className='text-primary font-bold mr-2'></FaMosque>
+                                <p className=''>Join The Islamic Community</p>
+                            </div>
+                            <div className="flex items-baseline">
+                                <FaMosque className='text-primary font-bold mr-2'></FaMosque>
+                                <p className=''>Join The Islamic Community</p>
+                            </div>
+                        </div>
+                        </div>
+                        <p className="">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Duis aute irure.
+                        </p>
+                       <div className="">
+                       <button class="btn btn-secondary">Learn More</button>
+                       </div>
                 </div>
 
 
                 <div className="right adviseImg ">
-                    <img className='w-full' src={img} alt="" />
+                    <img className='w-full h-full' src={img} alt="" />
                 </div>
             </div>
         </div>
