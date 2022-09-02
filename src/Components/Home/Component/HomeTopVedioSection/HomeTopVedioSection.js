312b7b15349311dcbfc21ea963cc82c271c948d6
anishazahan <anishazahan13@gmail.com>
2022-09-02 17:02:28 +0600

    home vedio section responsive

--- src/Components/Home/Component/HomeTopVedioSection/HomeTopVedioSection.js
+++ src/Components/Home/Component/HomeTopVedioSection/HomeTopVedioSection.js
@@ -9,24 +9,27 @@ const HomeTopVedioSection = () => {
     <div className='max-width mx-auto mt-30 lg:mt-36 mb-40'>
         <div className="px-10 xl:px-20 flex flex-col lg:flex-row">
 
-            <div className="left-vedio w-full  flex basis-5/12 relative mr-[-3rem]">
-               <div className="border-content absolute hidden lg:block">
+           
+        <div className="left-vedio w-full  flex basis-5/12 relative mr-[-6rem]">
+               <div className="border-content absolute hidden xl:block">
                <h4 className='watch-vedio-text font-bold'>WATCH OUR VIDEO</h4>
                </div>
-               <div className="w-full lg:w-4/5 mx-auto lg:mx-0 ml-auto">
-               <img className='img-vedio relative' src={img} alt="" />
+               <div className="w-full xl:w-4/5 mx-auto xl:mx-0 ml-auto">
+               <img className='img-vedio relative mx-auto lg:mx-0 xl:ml-24' src={img} alt="" />
                 <BsPlayCircle className='absolute play-btn'></BsPlayCircle>
                </div>
             </div>
 
-            <div className="right-content basis-8/12 lg:ml-24 ml-0 space-y-9">
+            <div className="right-content basis-8/12 lg:ml-36 ml-0 space-y-7 lg:space-y-6 xl:space-y-9">
 
                 <div className="heading w-full font-extrabold text-2xl tracking-wide text-gray-700">
                 <h2 className='mb-1 mt-14 lg:mt-0'>In The Name Of Allah </h2>
                 <h1> The Beneficent The Merciful</h1>
                 </div>
                 <img className='w-38' src={borderImg} alt="" />
-                <p className='text-gray-500 mb-16'>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin. This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean itudin. This is Photoshop's sion of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. This is Photoshop's versionf Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,</p>
+               <div className="">
+               <p className='text-gray-500 mb-10'>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin. This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean itudin. This is Photoshop's sion of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. This is Photoshop's versionf Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,</p>
+               </div>
                 <div className="flex">
                    <div className="vission space-y-5">
                    <div className="flex items-center space-x-4">
