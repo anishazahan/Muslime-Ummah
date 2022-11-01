cfd9f6977a7e119498bcfae51db34ca7e620ef3a
anishazahan <anishazahan13@gmail.com>
2022-11-01 18:56:15 +0600

    branch change

--- pages/home/Component/Banner/BannerBottomSection.js
+++ pages/home/Component/Banner/BannerBottomSection.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const BannerBottomSection = () => {
   return (
-    <div className='bg-slate-50 my-10'>
+    <div className='bg-slate-50 mb-10'>
         <div className="max-width mx-auto">
             <div className="flex flex-col lg:flex-row text-center lg:text-left px-8 md:px-5 lg:px-20 mx-auto">
                 <div className="left w-full space-y-7 my-16 ">
@@ -11,8 +11,6 @@ const BannerBottomSection = () => {
                     <p className='text-gray-500 w-full lg:w-4/5'>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin. This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean itudin. This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.</p>
                     <p className='text-sm'>~ by <span className='font-bold text-primary'>Akshay H.</span>  (Community Head)</p>
                 </div>
-               
-
                 <div className="right-img sm:w-full mx-5 lg:mx-0 lg:w-3/4 flex flex-col space-y-5 items-center justify-center h-full py-4  lg:py-0">
                    <div >
                    <h1 className="font-extrabold text-white text-xl mb-3">CONTACT US FOR A JOINING</h1>
