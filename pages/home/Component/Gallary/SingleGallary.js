ae810d73ca3d1d94072e39fc40fe1fc175aeb31c
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-11-03 19:03:13 +0600

    gellary seciton done

--- pages/home/Component/Gallary/SingleGallary.js
+++ pages/home/Component/Gallary/SingleGallary.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const SingleGallary = ({img}) => {
-    // console.log(img);
+const SingleGallary = ({ imgInfo }) => {
+  // console.log(img);
   return (
-  
+    <div className='w-[350px] my-5 rounded-md gellery-shadow'>
+      <img className='w-full p-1.5 rounded-md' src={imgInfo.url} alt="" />
+    </div>
 
-        <img src={img} alt="" />
 
-   
   )
 }
 
