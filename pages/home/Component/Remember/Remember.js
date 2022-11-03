b7e108f8dd01d41b9d9d665ca2b3c8d00d301f9a
anishazahan <anishazahan13@gmail.com>
2022-11-03 18:42:10 +0600

    remember sections img add

--- pages/home/Component/Remember/Remember.js
+++ pages/home/Component/Remember/Remember.js
@@ -2,20 +2,24 @@
 
 const Remember = () => {
   return (
-    <div className="remember-background px-20 text-white">
-        <div className="remember-left">
+    <section className="remember-background px-10 lg:px-20 text-gray-100 pt-[6%] 2xl:container mx-auto">
+        <div className="flex">
+        <div className="remember-left text-center lg:text-left lg:w-6/12">
         <h2 className='text-2xl md:text-3xl font-semibold tracking-wide'>Make Your Donation</h2>
-        <p>WEEKELY PROGRAMS FOR TAFSEER UL QURAN</p>
-        <p className="text-sm my-4">There are many variations of passages of Lorem Ipsum available, but the suffered alteration in some form, by injected humour, or randomised words but the majority have suffered alteration in some form, by injected humour, or randomised words...</p>
-        </div>
-        <div className="">
+        <p className="mt-2">WEEKELY PROGRAMS FOR TAFSEER UL QURAN</p>
+        <p className="text-sm my-4 mb-8 w-full">There are many variations of passages of Lorem Ipsum available, but the suffered alteration in some form, by injected humour, or randomised words but the majority have suffered alteration in some form, by injected humour, or randomised words...</p>
+        <div className="pt-4">
             <button>Learn More</button>
             <button>Learn More</button>
         </div>
-        <div className="remember-right"></div>
+        </div>
+        <div className="remember-right w-full lg:w-6/12 flex justify-end hidden lg:block">
+            <img className=" mt-[-5rem] lg:mt-[-8rem]" src={'http://kodeforest.net/html/islamic/images/pray.png'} alt="" />
+        </div>
+        </div>
 
 
-    </div>
+    </section>
   )
 }
 
