2a3e4079f36586102842f2713c24681c39c2216b
anishazahan <anishazahan13@gmail.com>
2022-11-04 11:52:57 +0600

    about sections image add

--- pages/home/Component/AboutIslam/AboutIslam.js
+++ pages/home/Component/AboutIslam/AboutIslam.js
@@ -6,14 +6,21 @@ const AboutIslam = () => {
 
       <div className="about-content lg:w-6/12">
 
+
+
       </div>
+
       <div className="about-img lg:w-6/12 flex space-x-3">
+
         <div className="flex flex-col space-y-3">
           <img src={'http://kodeforest.net/html/islamic/extra-images/mosque.jpg'} alt="" />
           <img src={"http://kodeforest.net/html/islamic/extra-images/mosque1.jpg"} alt="" />
         </div>
-        <div className="">
-          <img src={"http://kodeforest.net/html/islamic/extra-images/mosque2.jpg"} alt="" />
+        <div className="card">
+          <img className="card-img" src={"http://kodeforest.net/html/islamic/extra-images/mosque2.jpg"} alt="" />
+          <div className="card-info">
+            <h2 className="title font-medium">Madrasa in Istanbul</h2>
+          </div>
         </div>
       </div>
     </section>
