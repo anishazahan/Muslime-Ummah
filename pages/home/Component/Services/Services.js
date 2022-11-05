aad21a712c45530ea75c6d125d8043865f644b91
anishazahan <anishazahan13@gmail.com>
2022-11-05 23:44:52 +0600

    services sections react icon use

--- pages/home/Component/Services/Services.js
+++ pages/home/Component/Services/Services.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { FaQuran } from "react-icons/fa";
 
 
 const Services = () => {
@@ -14,12 +15,13 @@ const Services = () => {
         <img className="w-40 mx-auto" src={'http://kodeforest.net/html/islamic/images/hdg-img.png'} alt="" />
         <h1 className='text-3xl text-gray-800 my-5 text-center font-semibold'>OUR SERVICES</h1>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {
                 services?.map(service=>{
                     return <div className="service-card">
                        <div className="">
                        <img src={service.img} alt="" />
+                       <p><FaQuran></FaQuran></p>
                        <div className="pl-8 py-3 service-content">
                        <h2 className="font-semibold text-[19px] tracking-wide my-3">{service.name}</h2>
                        <p className="text-gray-500 text-sm mb-5">{service.time}</p>
