710a74eac8e4e885a4e7cfcedc5894d66d7016b9
anishazahan <anishazahan13@gmail.com>
2022-11-05 23:30:45 +0600

    services images

--- pages/home/Component/Services/Services.js
+++ pages/home/Component/Services/Services.js
@@ -14,11 +14,17 @@ const Services = () => {
         <img className="w-40 mx-auto" src={'http://kodeforest.net/html/islamic/images/hdg-img.png'} alt="" />
         <h1 className='text-3xl text-gray-800 my-5 text-center font-semibold'>OUR SERVICES</h1>
 
-            <div className="flex flex-wrap space-x-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {
                 services?.map(service=>{
-                    return <div className="w-[30%]">
-                            <h2>{service.name}</h2>
+                    return <div className="service-card">
+                       <div className="">
+                       <img src={service.img} alt="" />
+                       <div className="">
+                       <h2>{service.name}</h2>
+                       </div>
+                       </div>
+                            
                     </div>
                 })
             }
