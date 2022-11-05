815057c4cbd852df5b6abac0465fdf8d60d02410
anishazahan <anishazahan13@gmail.com>
2022-11-05 23:03:00 +0600

    services sections title add

--- pages/home/Component/Services/Services.js
+++ pages/home/Component/Services/Services.js
@@ -4,6 +4,7 @@ const Services = () => {
   return (
     <div className="my-20 mx-auto px-20 container">
         <img className="w-40 mx-auto" src={'http://kodeforest.net/html/islamic/images/hdg-img.png'} alt="" />
+        <h1 className='text-3xl text-gray-800 my-5 text-center font-semibold'>OUR SERVICES</h1>
 
 
     </div>
