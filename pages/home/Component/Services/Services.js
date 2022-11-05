2ec8894dbad01f09a8730b425e9cf42a2ef1cd40
anishazahan <anishazahan13@gmail.com>
2022-11-06 00:27:09 +0600

    update service component

--- pages/home/Component/Services/Services.js
+++ pages/home/Component/Services/Services.js
@@ -15,7 +15,6 @@ const Services = () => {
     <div className="my-20 mx-auto px-10 lg:px-20 container">
         <img className="w-40 mx-auto" src={'http://kodeforest.net/html/islamic/images/hdg-img.png'} alt="" />
         <h1 className='text-3xl text-gray-800 my-5 text-center font-semibold mb-10'>OUR SERVICES</h1>
-
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 gap-y-8 lg:gap-y-6 mx-auto">
             {
                 services?.map(service=>{
