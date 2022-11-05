f2ad18a9e21db57f9bf667399039381742251346
anishazahan <anishazahan13@gmail.com>
2022-11-05 14:43:32 +0600

    update aboutIslam sections images

--- pages/home/Component/AboutIslam/AboutIslam.js
+++ pages/home/Component/AboutIslam/AboutIslam.js
@@ -5,16 +5,25 @@ const AboutIslam = () => {
     <section className="mx-auto container px-20 flex flex-col lg:flex-row my-28">
 
       <div className="about-content lg:w-6/12">
-
-
-
       </div>
 
       <div className="about-img lg:w-6/12 flex space-x-3">
 
         <div className="flex flex-col space-y-3">
-          <img src={'http://kodeforest.net/html/islamic/extra-images/mosque.jpg'} alt="" />
-          <img src={"http://kodeforest.net/html/islamic/extra-images/mosque1.jpg"} alt="" />
+
+         <div className="relative img-container">
+         <img className="images duration-500" src={'http://kodeforest.net/html/islamic/extra-images/mosque.jpg'} alt="" />
+         <div className="img-info">
+            <h2 className="text-secondary font-semibold text-xl">Madrasa in Istanbul</h2>
+          </div>
+         </div>
+
+         <div className="relative img-container">
+         <img className="images duration-500" src={'http://kodeforest.net/html/islamic/extra-images/mosque1.jpg'} alt="" />
+         <div className="img-info">
+            <h2 className="text-secondary font-semibold text-xl">Madrasa in Istanbul</h2>
+          </div>
+         </div>
         </div>
         <div className="card">
           <img className="card-img" src={"http://kodeforest.net/html/islamic/extra-images/mosque2.jpg"} alt="" />
@@ -28,3 +37,4 @@ const AboutIslam = () => {
 }
 
 export default AboutIslam
+
