5ea536fad12df53694adca268adc241b3620e86b
anishazahan <anishazahan13@gmail.com>
2022-11-06 21:49:53 +0600

    service details components main ing styling

--- pages/services/serviceDetails.js
+++ pages/services/serviceDetails.js
@@ -1,16 +1,22 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
 const ServiceDetails = () => {
+    // const [services, setAllServices] = useState();
+    // useEffect(() => {
+    //   fetch("services.json")
+    //     .then((res) => res.json())
+    //     .then((data) => setAllServices(data));
+    // }, [services]);
   return (
-    <div className='my-56 container px-10'>
+    <div className='my-56 container px-20'>
 
         <div className="flex">
-         <div className="left-service">
-            
+         <div className="left-service w-2/3">
+            <img className="w-full h-[70%] object-cover" src={'http://kodeforest.net/html/islamic/extra-images/service2.jpg'} alt="" />
         </div>
 
 
-            <div className="right-service"></div>
+            <div className="right-service w-[30%]"></div>
         </div>
     </div>
   )
