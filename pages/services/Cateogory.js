6f1342363c6f4cc32397c5f3a79d0415559f89f7
anishazahan <anishazahan13@gmail.com>
2022-11-07 11:51:01 +0600

    blog pase add

--- pages/services/Cateogory.js
+++ pages/services/Cateogory.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Cateogory = () => {
   return (
-    <div className="bg-slate-100 flex flex-col pb-8">
+    <div className="bg-slate-100 flex flex-col pb-10">
         <div className="inline">
           <h2 className="font-semibold border-b-2 border-green-900 inline-block text-[17px] tracking-wide mt-[1.5rem] uppercase my-2 mx-10 mb-4">
         CATEGORIES
