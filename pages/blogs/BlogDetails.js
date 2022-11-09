79d7805fc880506b915b701b84de9e2f8bfccf4a
anishazahan <anishazahan13@gmail.com>
2022-11-09 15:41:36 +0600

    blogDetails component make

---
+++ pages/blogs/BlogDetails.js
@@ -0,0 +1,18 @@
+
+
+const BlogDetails = () => {
+  return (
+    <div className="container px-10 lg:px-20 mx-auto">
+
+        <div className="blogDetails-img">
+            <img src={'https://ihsan.templines.org/wp-content/uploads/2019/07/post004.jpg'} alt="" />
+        </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default BlogDetails
