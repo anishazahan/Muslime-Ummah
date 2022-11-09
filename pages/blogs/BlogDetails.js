558ab354a315e6acd0b03e7d14395f57959c5723
anishazahan <anishazahan13@gmail.com>
2022-11-09 15:51:33 +0600

    blog details sections img hover animation add

--- pages/blogs/BlogDetails.js
+++ pages/blogs/BlogDetails.js
@@ -4,8 +4,8 @@ const BlogDetails = () => {
   return (
     <div className="container px-10 lg:px-20 mx-auto my-28">
 
-        <div className="blogDetails-img w-[70%] relative">
-            <img className="w-full" src={'https://ihsan.templines.org/wp-content/uploads/2019/07/post004.jpg'} alt="" />
+        <div className="blogDetails-img w-[70%] relative overflow-hidden">
+            <img className="w-full h-full hover:scale-[1.2] duration-700" src={'https://ihsan.templines.org/wp-content/uploads/2019/07/post004.jpg'} alt="" />
             <div className="flex absolute left-4 right-0 bottom-7 space-x-2">
                 <button className="text-xs rounded text-white px-3 py-2 bg-primary">Prayer Service</button>
                 <button className="text-xs rounded text-white px-3 py-2 bg-primary">Ramdan</button>
