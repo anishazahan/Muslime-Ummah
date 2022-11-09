0c9c2ab707408e966e01adeedab9f362f71173da
anishazahan <anishazahan13@gmail.com>
2022-11-09 16:02:38 +0600

    blog details pages author,date,comment styling

--- pages/blogs/BlogDetails.js
+++ pages/blogs/BlogDetails.js
@@ -1,5 +1,7 @@
 
-
+import {FiUser } from "react-icons/fi";
+import {MdOutlineUpdate } from "react-icons/md";
+import {FaComment } from "react-icons/fa";
 const BlogDetails = () => {
   return (
     <div className="container px-10 lg:px-20 mx-auto my-28">
@@ -9,6 +11,21 @@ const BlogDetails = () => {
             <div className="flex absolute left-4 right-0 bottom-7 space-x-2">
                 <button className="text-xs rounded text-white px-3 py-2 bg-primary">Prayer Service</button>
                 <button className="text-xs rounded text-white px-3 py-2 bg-primary">Ramdan</button>
+            </div>
+        </div>
+
+        <div className="flex my-4 space-x-6">
+            <div className="flex items-center space-x-1">
+                <p><FiUser className=" text-primary font-semibold"></FiUser></p>
+                <h2 className="text-xs text-gray-500"> By Zaymund Alex </h2>
+            </div>
+            <div className="flex items-center space-x-1">
+                <p><MdOutlineUpdate className=" text-primary font-semibold"></MdOutlineUpdate></p>
+                <h2 className="text-xs text-gray-500"> July 30, 2019 </h2>
+            </div>
+            <div className="flex items-center space-x-1">
+                <p><FaComment className=" text-black font-semibold"></FaComment></p>
+                <h2 className="text-xs text-gray-500"> 1 comments </h2>
             </div>
         </div>
 
