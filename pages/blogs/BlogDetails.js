afb8491a9a574697241410b3c8005c7727175cd8
anishazahan <anishazahan13@gmail.com>
2022-11-09 17:15:06 +0600

    blof details pages other description add

--- pages/blogs/BlogDetails.js
+++ pages/blogs/BlogDetails.js
@@ -13,7 +13,6 @@ const BlogDetails = () => {
                 <button className="text-xs rounded text-white px-3 py-2 bg-primary">Ramdan</button>
             </div>
         </div>
-
         <div className="flex my-4 space-x-6">
             <div className="flex items-center space-x-1">
                 <p><FiUser className=" text-primary font-semibold"></FiUser></p>
@@ -29,10 +28,17 @@ const BlogDetails = () => {
             </div>
         </div>
         <div className="">
-            <p className="text-sm text-gray-600">Moveth the seed you waters is replenish light one meat. Under divide image likeness their may, form Earth evening moving. Heaven was have upon god place dry one creepeth one days divided. For fruitful blessed void days seasons wherein dry She’d light creeping that grass, is rule rule. Waters midst i him lesser.</p>
+            <p className="text-sm text-gray-600 mb-3">Moveth the seed you waters is replenish light one meat. Under divide image likeness their may, form Earth evening moving. Heaven was have upon god place dry one creepeth one days divided. For fruitful blessed void days seasons wherein dry She’d light creeping that grass, is rule rule. Waters midst i him lesser.</p>
         </div>
-        <div className="">
-            
+        <div className="flex lg:space-x-4 items-stretch">
+            <div className="left-content w-full mx-autolg:w-[70%] ">
+            <p className="text-sm text-gray-600">Evening together all was night fourth gathered. Whales is form. Deep blessed. Every Have good and. Called every. You’ll his void fourth is fifth it greater seasons place, signs they’re days let his meat, tree which let signs light night stars his third .</p>
+            <p className="text-sm text-gray-600 my-3 ">Evening together all was night fourth gathered. Whales is form. Deep blessed. Every Have good and. Called every. You’ll his void fourth is fifth it greater seasons place, signs they’re days let his meat, tree which let signs light night stars his third .</p>
+            <p className="text-sm text-gray-600 mb-3 ">So bring Moveth seasons kind made over let. His. They’re Our moved, forth evening. Beginning together. Living deep be were own you fifth under. Us under. Him there second so. You’re bearing evening seas man first. Place second be, may stars brought darkness shall heaven green days, upon multiply beginning male air. Fruitful him isn’t day. Yielding fill. He saw replenish winged stars earth days void heaven, female which is. Bring creeping you’re stars gathered. Said second open two can’t divide..</p>
+            </div>
+            <div className="right-content w-full mx-auto lg:w-[30%]">
+                <img src={'https://ihsan.templines.org/wp-content/uploads/2019/08/Service-Box-Image-1-300x200.jpg'} alt="" />
+            </div>
         </div>
 
 
