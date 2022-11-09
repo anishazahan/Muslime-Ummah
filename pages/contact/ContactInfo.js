82aabeb5e40e49df50946ea2d9d0de7cd4dccb7b
anishazahan <anishazahan13@gmail.com>
2022-11-09 11:47:52 +0600

    contactInfo sections react icon add

--- pages/contact/ContactInfo.js
+++ pages/contact/ContactInfo.js
@@ -1,10 +1,16 @@
-
+import { MdLocationPin } from "react-icons/md";
 
 const ContactInfo = () => {
   return (
     <section className="my-28">
-        <div className="flex">
-            
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-20 mx-auto">
+            <div className="contactInfo-box">
+            <div className="flex items-center">
+                <p><MdLocationPin className='text-4xl text-primary'></MdLocationPin></p>
+                <h2 className="">Our location</h2>
+            </div>
+            </div>
+
         </div>
     
     
