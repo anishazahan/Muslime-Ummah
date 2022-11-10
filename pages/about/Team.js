376e52dbdd22f4361755e517c01f84270bad896e
anishazahan <anishazahan13@gmail.com>
2022-11-11 05:58:38 +0600

    load data from store and styling team sections img

--- pages/about/Team.js
+++ pages/about/Team.js
@@ -7,11 +7,15 @@ const Team = () => {
   return (
     <div className="container mx-auto">
         <h2 className="flex justify-center text-xl font-semibold my-20 lg:text-3xl">Our Amazing Team</h2>
-       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-10 lg:px-20">
+       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-10 lg:px-20 ">
        {
-          teams.map(team=>{
-            return <div className="">
-              
+          teams?.map(team=>{
+            return <div className="text-center">
+                <div className="  w-[85%] mx-auto">
+                  <img className="w-full" src={team.img} alt="" />
+                </div>
+                <h2>{team.name}</h2>
+                <h2>{team.situation}</h2>
             </div>
 
           })
