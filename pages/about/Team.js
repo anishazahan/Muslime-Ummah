4514cbc6a0d33c59814c7199c36535e37d8e2fea
anishazahan <anishazahan13@gmail.com>
2022-11-11 05:48:32 +0600

    data load from redux store for team section

--- pages/about/Team.js
+++ pages/about/Team.js
@@ -1,12 +1,22 @@
 import { useSelector } from "react-redux"
 
 const Team = () => {
-  const team = useSelector(state => state.team.team)
+  const teams = useSelector(state => state.team.team)
     //  console.log(team);
 
   return (
     <div className="container mx-auto">
         <h2 className="flex justify-center text-xl font-semibold my-20 lg:text-3xl">Our Amazing Team</h2>
+       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-10 lg:px-20">
+       {
+          teams.map(team=>{
+            return <div className="">
+              
+            </div>
+
+          })
+        }
+       </div>
       
     </div>
   )
