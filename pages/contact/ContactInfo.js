1937f972dd656d425dac519b74b67f1627518c45
anishazahan <anishazahan13@gmail.com>
2022-11-12 06:23:40 +0600

    responsive contact info box

--- pages/contact/ContactInfo.js
+++ pages/contact/ContactInfo.js
@@ -1,16 +1,16 @@
 import { MdLocationPin } from "react-icons/md";
-import { BsFillTelephoneInboundFill } from "react-icons/bs";
+import { BsFillTelephoneInboundFill,BsFillEnvelopeFill } from "react-icons/bs";
 
 
 const ContactInfo = () => {
   return (
-    <section className="my-28">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-20 mx-auto space-x-9">
+    <section className="my-20 container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-6  md:px-20 mx-auto space-y-4 lg:space-y-0 space-x-9">
     {/* ///////------c
     location/////// */}
 
         <div className="contactInfo-box py-12 hover:bg-slate-200 duration-500 cursor-pointer">
-            <div className="flex items-center border-green-800 mx-12 border-dashed border-b-2 justify-center">
+            <div className="flex space-x-2 items-center border-green-800 mx-12 border-dashed border-b-2 justify-center">
                 <p><MdLocationPin className='text-4xl text-primary'></MdLocationPin></p>
                 <h2 className="text-2xl font-semibold">Our location</h2>
             </div>
@@ -23,7 +23,7 @@ const ContactInfo = () => {
             {/* ///////------
         Phone/////// */}
             <div className="contactInfo-box py-12 hover:bg-slate-200 duration-500 cursor-pointer">
-            <div className="flex items-center border-green-800 mx-12 border-dashed border-b-2 justify-center">
+            <div className="flex items-center space-x-2 border-green-800 mx-12 border-dashed border-b-2 justify-center">
                 <p><BsFillTelephoneInboundFill className='text-2xl text-primary'></BsFillTelephoneInboundFill></p>
                 <h2 className="text-2xl font-semibold">Phone Us</h2>
             </div>
@@ -35,9 +35,9 @@ const ContactInfo = () => {
 
             {/* ///////------
                       Email us/////// */}
-            <div className="contactInfo-box py-12 hover:bg-slate-200 duration-500 cursor-pointer">
-            <div className="flex items-center border-green-800 mx-12 border-dashed border-b-2 justify-center">
-                <p><BsFillTelephoneInboundFill className='text-2xl text-primary'></BsFillTelephoneInboundFill></p>
+            <div className="contactInfo-box py-12 hover:bg-slate-200 duration-500 cursor-pointer text-center">
+            <div className="flex items-center space-x-2 border-green-800 mx-12 border-dashed border-b-2 justify-center">
+                <p><BsFillEnvelopeFill className='text-2xl text-primary'></BsFillEnvelopeFill></p>
                 <h2 className="text-2xl font-semibold">Email Us</h2>
             </div>
             <div className="text-center mt-8">
---
+++ pages/contact/ContactMessage.js
@@ -0,0 +1,14 @@
+
+
+const ContactMessage = () => {
+  return (
+    <section>
+            <div className="flex flex-col lg:flex-row">
+
+            </div>
+
+    </section>
+  )
+}
+
+export default ContactMessage
