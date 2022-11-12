c07e8e7e53f2ecfd3b63502f3d50d17453be6a6b
anishazahan <anishazahan13@gmail.com>
2022-11-12 10:36:45 +0600

    google map image add in contact us page

--- pages/contact/ContactMessage.js
+++ pages/contact/ContactMessage.js
@@ -4,7 +4,7 @@ const ContactMessage = () => {
   return (
     <section className="container lg:px-20 px-10 mx-auto mb-20">
       <h2 className="text-2xl font-semibold mb-10">Send Your message</h2>
-            <div className="flex flex-col lg:flex-row">
+            <div className="flex flex-col lg:flex-row justify-center items-stretch lg:space-x-5">
               <div className="left w-6/12">
                 <form>
                 <div className="w-full">
@@ -26,14 +26,16 @@ const ContactMessage = () => {
                 <div className="w-full">
              <textarea  className="border border-2 mb-8 px-5 w-full py-3 border-gray-300 placeholder-black outline-none" name="" id="" cols="30" rows="10" placeholder="Your Message*"></textarea>
             </div>
-            <div className="">
-              <input className="py-2 px-10 bg-primary text-white" type="button" value="Submit" />
+            <div className="lg:w-full">
+              <input className="py-2 px-10 bg-primary w-full text-white" type="button" value="Submit" />
             </div>
 
                 </form>
               </div>
-              <div className="right w-612">
-                <img className="w-full" src={''} alt="" />
+              <div className="right w-6/12">
+               <div className="w-full h-full">
+               <img className="h-full object-cover" src={'https://d32ogoqmya1dw8.cloudfront.net/images/sp/library/google_earth/google_maps_hello_world.jpg'} alt="" />
+               </div>
               </div>
             </div>
 
