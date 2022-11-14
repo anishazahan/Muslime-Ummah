1b9d010af9618da8c6f4072cfe36b82dbf1916d9
anishazahan <anishazahan13@gmail.com>
2022-11-14 19:01:20 +0600

    update pillar of islam section

--- pages/home/Component/PillerOfIslam/PillerOfIslam.js
+++ pages/home/Component/PillerOfIslam/PillerOfIslam.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const PillerOfIslam = () => {
   return (
-    <section className='mb-20 mt-14 xl:container mx-auto'>
+    <section className='pb-28 pt-20 xl:container mx-auto bg-green-100'>
         <div className="text-center space-y-6">
             <h2 className='font-semibold text-[18px] text-primary'>Islamic Pillers</h2>
             <h1 className='text-3xl font-semibold'>5 Islamic Pillers of Islam</h1>
@@ -13,29 +13,29 @@ const PillerOfIslam = () => {
         </div>
 
         <div className=" mt-8 cards grid grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-2 lg:grid-cols-5 mx-auto max-width px-5 lg:px-20">
-            <div className="pillerOfIslamCard border-none text-center px-12 lg:px-16 shrink-0 rounded-none py-12 space-y-4 hover:space-y-5 ease-in duration-300 ">
+            <div className=" bg-white  hover:scale-[1.1]  pillerOfIslamCard border-none text-center px-12 lg:px-16 shrink-0 rounded-none py-12 space-y-4 ease-in duration-500 ">
                 <img className='piller-img' src={"https://i.ibb.co/qkKnCyH/piller-Img1.png"} alt="" />
                 <h1 className="text-xl font-semibold">Shahada</h1>
                 <p className="text-primary text-xs font-medium">(faith)</p>
             </div>
-            <div className="pillerOfIslamCard  border-none text-center px-12 lg:px-16 shrink-0 px-20 py-12 space-y-4  rounded-none ">
+            <div className="bg-white duration-500  hover:scale-[1.1] pillerOfIslamCard  border-none text-center lg:px-16 shrink-0 px-20 py-12 space-y-4  rounded-none ">
                 <img className='piller-img' src={"https://i.ibb.co/yqbY3gm/piller-Islam2.png"} alt="" />
                 <h1 className="text-xl font-semibold">Shahada</h1>
                 <p className="text-primary text-xs font-medium">(faith)</p>
             </div>
         
-            <div className="pillerOfIslamCard  border-none text-center px-12 lg:px-16 shrink-0 px-20 py-12 space-y-4  rounded-none  ">
+            <div className="bg-white duration-500  hover:scale-[1.1] pillerOfIslamCard  border-none text-center lg:px-16 shrink-0 px-20 py-12 space-y-4  rounded-none  ">
                 <img className='piller-img' src={"https://i.ibb.co/g3yCyf4/piller-Islam3.png"} alt="" />
                 <h1 className="text-xl font-semibold">Shahada</h1>
                 <p className="text-primary text-xs font-medium">(faith)</p>
             </div>
 
-            <div className="pillerOfIslamCard  border-none text-center px-12 lg:px-16 shrink-0 px-20 py-12 space-y-4  rounded-none  ">
+            <div className="bg-white duration-500  hover:scale-[1.1] pillerOfIslamCard  border-none text-center lg:px-16 shrink-0 px-20 py-12 space-y-4  rounded-none  ">
                 <img className='piller-img' src={"https://i.ibb.co/ft8GQNH/piller-Islam4.png"} alt="" />
                 <h1 className="text-xl font-semibold">Shahada</h1>
                 <p className="text-primary text-xs font-medium">(faith)</p>
             </div>
-            <div className="pillerOfIslamCard  border-none text-center px-12 lg:px-16 shrink-0 px-20 py-12 space-y-4  rounded-none  ">
+            <div className="bg-white duration-500  hover:scale-[1.1] pillerOfIslamCard  border-none text-center lg:px-16 shrink-0 px-20 py-12 space-y-4  rounded-none  ">
                 <img className='piller-img' src={"https://i.ibb.co/ZMPw561/pillerislam5.png"} alt="" />
                 <h1 className="text-xl font-semibold">Shahada</h1>
                 <p className="text-primary text-xs font-medium">(faith)</p>
